8c55f86f36ccfd87400e021eb8b224d644116633
robertocannella <robertocannella@gmail.com>
2022-08-07 15:07:53 -0400

    Add trig functions to arc.js

--- arc.js
+++ arc.js
@@ -16,7 +16,7 @@ const originY = width / 2;
 let currentQuadrant = 1;
 let currentX = scale(Math.sqrt(2), marginX, width - marginX, -1.5, 1.5);
 let currentY = scale(2, marginY, height - marginY, 1.5, -1.5);
-let radius = scale(1,-1.5, 1.5, 0, width / 2 - marginX)
+let radius = scale(1, -1.5, 1.5, 0, width / 2 - marginX)
 let initial = 0
 let startAngle = (Math.PI * initial / 180) - Math.PI * 2
 let endAngle = Math.atan(currentY / currentX) - Math.PI * 2
@@ -29,38 +29,69 @@ const dragCircle = d3.drag()
         d3.select(this).attr('cursor', 'move');
     })
     .on('drag', function (event) {
+        // Get new coordinates here and scale to chart
         const newX = scale(event.x, marginX, width - marginX, -1.5, 1.5);
         const newY = scale(event.y, marginY, height - marginY, 1.5, -1.5);
 
+        // Set the new angle depending of the quadrant
         let endAngle = - Math.atan(newY / newX) - Math.PI * 2
-        if (newX > 0 && newY > 0) {
+        if (newX > 0 && newY > 0) { // quadrant 1
             endAngle = - Math.atan(newY / newX) - Math.PI * 2
         }
-        if (newX < 0 && newY > 0) {
+        if (newX < 0 && newY > 0) { // quadrant 2
             endAngle = - Math.atan(newY / newX) - Math.PI * 2 - (Math.PI)
         }
-        if (newX < 0 && newY < 0) {
-
+        if (newX < 0 && newY < 0) { // quadrant 3
             endAngle = - Math.atan(newY / newX) - Math.PI * 2 - (Math.PI)
         }
-        if (newX > 0 && newY < 0) {
+        if (newX > 0 && newY < 0) { // quadrant 4
             endAngle = - Math.atan(newY / newX) - Math.PI * 2 - (Math.PI * 2)
         }
+
+        // Generate a new ARC path
         const arc = d3.arc()
-            .innerRadius(10)
+            .innerRadius(15)
             .outerRadius(radius)
             .startAngle(startAngle)
             .endAngle(endAngle)
 
-        d3.select(this).attr('cx', () => - radius * Math.cos(endAngle));
-        d3.select(this).attr('cy', () => - radius * Math.sin(endAngle))
-            .attr('transform', `translate(${originX},${originY}) rotate(180)`)
-
-        console.log(newX, newY, endAngle)
         d3.select('.arc-path')
             .datum(arc)
             .attr('d', (d) => { return d })
             .attr('transform', `translate(${originX},${originY}) rotate(${90})`)
+
+        // Move Point
+        d3.select(this).attr('cx', () => - radius * Math.cos(endAngle));
+        d3.select(this).attr('cy', () => - radius * Math.sin(endAngle))
+            .attr('transform', `translate(${originX},${originY}) rotate(180)`)
+
+
+        // Animate Trig Functions Here
+        // Move Ray
+        const x = radius * Math.cos(endAngle)
+        const y = radius * Math.sin(endAngle)
+        const sine = [
+            [x, 0],
+            [x,y]
+        ];
+        const cosine = [
+            [x, y],
+            [0, y]
+        ];
+        const hypotenuse = [
+            [0, 0],
+            [x, y]
+        ];
+
+        const pathOfLine = Gen(hypotenuse);
+        const sinLine = Gen(sine);
+        const cosineLine = Gen(cosine);
+        d3.select('.sine')
+            .attr('d', sinLine)
+        d3.select('.cosine')
+            .attr('d', cosineLine);
+        d3.select('.ray')
+            .attr('d', pathOfLine);
     })
     .on('end', function (d, i, nodes) {
         d3.select(this).attr('cursor', 'auto')
@@ -121,15 +152,8 @@ svg.append("g")
     .call(yNegAxis);
 
 
-// const arc = d3.arc()
-//     .innerRadius(10)
-//     .outerRadius(radius)
-//     .startAngle(Math.PI * startAngle / 180)
-//     .endAngle(Math.PI * endAngle / 180)
-
-
 const arc = d3.arc()
-    .innerRadius(10)
+    .innerRadius(15)
     .outerRadius(radius)
     .startAngle(startAngle)
     .endAngle(endAngle)
@@ -143,9 +167,74 @@ svg.append('path')
     .attr('stroke', 'green')
 
 svg.append('circle')
-    .attr('cx', () => radius * Math.sin(endAngle))
-    .attr('cy', () => radius * Math.cos(endAngle))
+    .attr('cx', () => radius * Math.cos(endAngle))
+    .attr('cy', () => radius * Math.sin(endAngle))
     .attr('r', '6px')
     .attr('class', 'ray-point')
-    .attr('transform', `translate(${originX},${originY}) rotate(180)`)
+    .attr('transform', `translate(${originX},${originY})`)
     .call(dragCircle)
+
+
+// Create the Ray
+// Making a line Generator
+
+const mappedX = radius * Math.cos(endAngle)
+const mappedY = radius * Math.sin(endAngle)
+const Gen = d3.line();
+
+
+const hypotenuse = [
+    [0, 0],
+    [mappedX, mappedY]
+];
+
+const sine = [
+    [mappedX, mappedY],
+    [mappedX, 0]
+];
+
+const cosine = [
+    [mappedX, mappedY],
+    [0, mappedY]
+];
+const initialRay =[
+    [mappedX, 0],
+    [0,0]
+]
+const pathOfLine = Gen(hypotenuse);
+const sinLine = Gen(sine);
+const cosineLine = Gen(cosine);
+const initialRayLine = Gen(initialRay)
+
+svg.append('path')
+    .attr('d', sinLine)
+    .attr('class', 'sine')
+    .attr('transform', `translate(${originX},${originY})`)
+svg.append('path')
+    .attr('d', cosineLine)
+    .attr('class', 'cosine')
+    .attr('transform', `translate(${originX},${originY})`)
+svg.append('path')
+    .attr('d', pathOfLine)
+    .attr('class', 'ray')
+    .attr('transform', `translate(${originX},${originY})`)
+svg.append('path')
+    .attr('d', initialRayLine)
+    .attr('class', 'ray')
+    .attr('transform', `translate(${originX},${originY})`)
+
+// This is the full circle
+
+const fullArc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius)
+    .startAngle(0)
+    .endAngle(Math.PI * 2)
+
+svg.append('path')
+    .datum(fullArc)
+    .attr('d', (d) => { return d })
+    .attr('transform', `translate(${originX},${originY}) rotate(90)`)
+    .attr('fill', 'none')
+    .attr('class', 'arc-path')
+    .attr('stroke', 'green')
